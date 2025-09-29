7baba07459ad7f7e625e8aa6e48c1ca36259462d
vnazarov <v.nazarov@atlantic-intermodal.com>
2025-09-29 10:39:20 +0300

    Fix baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  app: {
-    baseURL: '/fsm-slot-machine/'
-  }
+  css: ['~/assets/css/main.css']
 })
